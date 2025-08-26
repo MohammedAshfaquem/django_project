e1a7fa524d05fc4dd86d54d336ae3aab05efa978
Mohammed Ashfaque <mohammedashfaquemvkpadi@gmail.com>
2025-08-26 12:13:20 +0530

    problem fixed

--- app/models.py
+++ app/models.py
@@ -11,14 +11,14 @@ class User(models.Model):
         return self.name
 
     
+    
 class Note(models.Model):
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,default='untitled')
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    
     def __str__(self):
         return self.title
-    
+   
 # Create your models here.
--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('home/', views.home, name='home'),
-    # path('add/', views.add_note, name='add_note'),
-    # path('edit/<int:note_id>/', views.edit_note, name='edit_note'),
-    # path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
+
 ]
